a7ce3b1fa60247ecb82b351c1cd2f634e2c010d9
otaviozerotwo <deadplatypus@protonmail.com>
2023-07-01 20:40:34 -0300

    implementação smoke test

---
+++ test/FabricaConexaoSmokeTest.java
@@ -0,0 +1,64 @@
+import static org.junit.Assert.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import pacote.DAO.FabricaConexao;
+
+public class FabricaConexaoSmokeTest {
+    private Connection conexao;
+
+    @Before
+    public void setUp() {
+        // Arrange
+        conexao = FabricaConexao.conexao();
+    }
+
+    @After
+    public void tearDown() {
+        // Limpeza após cada teste, fechando a conexão
+        if (conexao != null) {
+            try {
+                conexao.close();
+            } catch (SQLException ex) {
+                fail("Erro ao fechar a conexão: " + ex.getMessage());
+            }
+        }
+    }
+
+    @Test
+    public void testConexaoNotNull() {
+        // Assert
+        assertNotNull(conexao);
+    }
+
+    @Test
+    public void testConexaoIsOpen() {
+        try {
+            // Assert
+            assertFalse(conexao.isClosed());
+        } catch (SQLException ex) {
+            fail("Erro ao verificar o status da conexão: " + ex.getMessage());
+        }
+    }
+
+    @Test
+    public void testConsultaSimples() {
+        try {
+            // Arrange
+            Statement stmt = conexao.createStatement();
+
+            // Act
+            ResultSet resultSet = stmt.executeQuery("SELECT 1");
+
+            // Assert
+            assertTrue(resultSet.next());
+            assertEquals(1, resultSet.getInt(1));
+        } catch (SQLException ex) {
+            fail("Erro ao executar a consulta: " + ex.getMessage());
+        }
+    }
+}
